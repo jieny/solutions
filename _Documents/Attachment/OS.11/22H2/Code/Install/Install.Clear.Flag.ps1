f032435cdf2e7e16414cfaaca32b8a0cac8c2c69
Yi <73377514+ilikeyi@users.noreply.github.com>
2023-09-30 02:50:08 +0800

    docs

---
+++ _Documents/Attachment/OS.11/22H2/Code/Install/Install.Clear.Flag.ps1
@@ -0,0 +1,15 @@
+﻿$Lang = "zh-CN"
+
+Get-AppXProvisionedPackage -Path "D:\OS11_Custom\Install\Install\Mount" | Foreach-object {
+    if ($_.DisplayName -Like "*LanguageExperiencePack*$($Lang)*") {
+        Write-host "   $($_.DisplayName)"
+        Write-Host "   Deleting".PadRight(22) -NoNewline
+        
+        try {
+            Remove-AppxProvisionedPackage -Path "D:\OS11_Custom\Install\Install\Mount" -PackageName $_.PackageName -ErrorAction SilentlyContinue | Out-Null
+            Write-host "Finish" -ForegroundColor Green
+        } catch {
+            Write-host "Failed" -ForegroundColor Red
+        }
+    }
+}
