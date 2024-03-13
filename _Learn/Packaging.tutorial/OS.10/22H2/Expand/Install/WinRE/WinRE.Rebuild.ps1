51b23639f569fae98454ae2a7ba8cad9faacf537
Yi <73377514+ilikeyi@users.noreply.github.com>
2024-03-13 14:34:07 +0800

    f

---
+++ _Learn/Packaging.tutorial/OS.10/22H2/Expand/Install/WinRE/WinRE.Rebuild.ps1
@@ -0,0 +1,21 @@
+﻿# WinRE 文件路径
+$FileName = "D:\OS_10_Custom\Install\Install\Mount\Windows\System32\Recovery\WinRE.wim"
+
+# 获取映像来源
+Get-WindowsImage -ImagePath $Filename -ErrorAction SilentlyContinue | ForEach-Object {
+    Write-Host "   Image name: " -NoNewline; Write-Host "$($_.ImageName)" -ForegroundColor Yellow
+    Write-Host "   The index number: " -NoNewline; Write-Host "$($_.ImageIndex)" -ForegroundColor Yellow
+    Write-Host "`n   Under reconstruction ".PadRight(28) -NoNewline
+    Export-WindowsImage -SourceImagePath "$($Filename)" -SourceIndex "$($_.ImageIndex)" -DestinationImagePath "$($FileName).New" -CompressionType max 
+    Write-Host "Finish`n" -ForegroundColor Green
+}
+
+# 判断导出状态
+if (Test-Path "$($FileName).New" -PathType Leaf) {
+    Remove-Item -Path $Filename
+    Move-Item -Path "$($FileName).New" -Destination $Filename
+    Write-Host "Finish" -ForegroundColor Green
+} else {
+    Write-host "Failed" -ForegroundColor Red
+    Write-host "   $($_)" -ForegroundColor Red
+}
