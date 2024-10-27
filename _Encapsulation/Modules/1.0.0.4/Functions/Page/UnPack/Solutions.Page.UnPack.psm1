9fde52c8c4b10a815fed7535e8dfdac86fc30d45
ilikeyi <775159955@qq.com>
2024-10-27 17:05:22 +0800

    1.0.0.4

--- _Encapsulation/Modules/1.0.0.4/Functions/Page/UnPack/Solutions.Page.UnPack.psm1
+++ _Encapsulation/Modules/1.0.0.4/Functions/Page/UnPack/Solutions.Page.UnPack.psm1
@@ -885,12 +885,7 @@ Function Update_Create_Version
 	},
 	"changelog": {
 		"title": "$((Get-Module -Name Solutions).Author)'s Solutions - Change log",
-		"log":   "   - Added Windows 11 24H2 autonomous driving solutions, packaging tutorials, video tutorials, rules, etc. * New
-   - Added Windows Server 2025 autonomous driving solutions, rules, etc. * New
-   - Windows 11 22H2: Obsolete ones removed * Del"
-   - Windows 11 23H2: Update packaging tutorial and update the binding area when installing InBox Apps applications
-   - InBox Apps: Add region tags during installation * New
-   - InBox Apps: Allow automatic combination of dependent packages during installation * New
+		"log":   "   - Added Windows 11 24H2 autonomous driving solutions, packaging tutorials, video tutorials, rules, etc. * New\n   - Added Windows Server 2025 autonomous driving solutions, rules, etc. * New\n   - Windows 11 22H2: Obsolete ones removed * Del\n   - Windows 11 23H2: Update packaging tutorial and update the binding area when installing InBox Apps applications\n   - InBox Apps: Add region tags during installation * New\n   - InBox Apps: Allow automatic combination of dependent packages during installation * New"
 	},
 	"url": "$((Get-Module -Name Solutions).HelpInfoURI)/download/solutions/latest.zip"
 }
